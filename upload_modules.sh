80b2ea79bf407cbcab2ede44e501cc00e764e227
Yang Ge <yangge@broadcom.com>
2014-01-03 16:54:54 -0800

    use -v option for scp

--- upload_modules.sh
+++ upload_modules.sh
@@ -19,4 +19,4 @@ cd ${module_dir}
 cp ${kernel_dir}/arch/arm/boot/Image ${module_dir}/lib
 binary_new="binary_new.tar.gz"
 tar -czvf ${binary_new} ./lib
-scp ${binary_new} pi@192.168.1.10:${binary_new}
+scp ${binary_new} pi@192.168.1.10:${binary_new} -v
