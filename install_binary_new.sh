5a8ddbd2980e5a8d89aa55f9e59fc7a9ec094ebe
Yang Ge <eroicaleo@gmail.com>
2014-01-20 03:49:06 +0000

    Updated install_binary_new.sh

--- install_binary_new.sh
+++ install_binary_new.sh
@@ -1,27 +1,31 @@
 #!/bin/bash                                                                                                                   
 echo "Start installing..."
 HOME_DIR="/home/pi"
+cd ../
+PROJ_DIR=$(pwd)
+echo $PROJ_DIR
+MODULE_DIR="$PROJ_DIR/modules"
 # Install modules
-if [[ -e ${HOME_DIR}/lib/modules ]]; then
+if [[ -e ${MODULE_DIR}/lib/modules ]]; then
   echo "Install modules..."
   rm -rf /lib/modules_new /lib/modules
-  cp -rf ${HOME_DIR}/lib/modules /lib/modules_new
-  ln -sf /lib/modules_new /lib/modules
+  # cp -rf ${PROJ_DIR}/lib/modules /lib/modules_new
+  ln -sf ${MODULE_DIR}/lib/modules /lib/modules
 fi
 
 # Install firmware
-if [[ -e ${HOME_DIR}/lib/firmware ]]; then
+if [[ -e ${MODULE_DIR}/lib/firmware ]]; then
   echo "Install firmware..."
   rm -rf /lib/firmware_new /lib/firmware
-  cp -rf ${HOME_DIR}/lib/firmware /lib/firmware_new
-  ln -sf /lib/firmware_new /lib/firmware
+  # cp -rf ${PROJ_DIR}/lib/firmware /lib/firmware_new
+  ln -sf ${MODULE_DIR}/lib/firmware /lib/firmware 
 fi
 
 # Install kernel image
-if [[ -e ${HOME_DIR}/lib/Image ]]; then
+if [[ -e ${MODULE_DIR}/lib/Image ]]; then
   echo "Install kernel image..."
   rm -rf /boot/kernel_new.img
-  cp -rf ${HOME_DIR}/lib/Image /boot/kernel_new.img
+  cp -rf ${MODULE_DIR}/lib/Image /boot/kernel_new.img
   # edit file in place
   sudo sed -i 's/# kernel=kernel_new.img/  kernel=kernel_new.img/' /boot/config.txt
 fi
